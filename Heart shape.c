7888d4b9d578b04f92c3b17da4cc20290981dd3c
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-05-30 21:46:35 +0530

    Add files via upload

---
+++ Heart shape.c
@@ -0,0 +1,61 @@
+/*
+
+output for n = 5
+
+    *         * 
+   * *       * * 
+  * * *     * * * 
+ * * * *   * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+ * * * * * * * * * 
+  * * * * * * * * 
+   * * * * * * * 
+    * * * * * * 
+     * * * * * 
+      * * * * 
+       * * * 
+        * * 
+         * 
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  printf("enter number of rows : ");
+  scanf("%d",&n);
+  
+  for( int i = 1 ; i <=n ; i++ ){
+    for ( int space = 1 ; space <= (n-i) ; space++ ){
+      printf(" ");
+    }
+    for ( int j = 1 ; j <= i ; j++){
+      printf("* ");
+    }
+
+    
+    for ( int space = 1 ; space <= 2*(n-i) ; space++ ){
+      printf(" ");
+    }
+    for ( int j = 1 ; j <= i ; j++){
+      printf("* ");
+    }
+    printf("\n");
+  }
+  n = 2*n ;
+  for (int i = n ; i >= 1 ; i-- ){
+    for (int space = 0 ; space < n-i ; space++){
+      printf(" ");
+    }
+    for ( int j = i ; j >= 1 ; j--){
+      printf("* ");
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
