c662d2522725d9eacb3e3f22993420b198b2b5cd
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-06-08 20:46:04 +0530

    Add files via upload

---
+++ hourglass.c
@@ -0,0 +1,49 @@
+/*
+output --->
+
+enter value for number of rows : 5
+
+ * * * * *
+  *     *
+   *   *
+    * *
+     *
+    * *
+   *   *
+  *     *
+ * * * * *
+
+
+*/
+
+
+#include <stdio.h>
+
+int main(){
+    int n , m ;
+    printf("enter value for number of rows : ");
+    scanf("%d", &n );
+    printf("\n");
+
+
+    for (int i = n ; i >= 1 ; i-- ){
+        for ( int k = n ; k >= i ; k-- ){
+            printf(" ");
+        }
+        for(int j = 1 ; j <= i ; j++ ){
+                printf("* ");
+        }
+        printf("\n");
+    }
+    for (int i = 2 ; i <= n ; i++ ){
+        for ( int k = n ; k >= i ; k-- ){
+            printf(" ");
+        }
+        for(int j = 1 ; j <= i ; j++ ){
+                printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0 ;
+}
